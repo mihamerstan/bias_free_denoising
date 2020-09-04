8c299cc923798d9391ac5a5d721ff3f4f88c0ae0
Michael <michael.hamer.stanley@gmail.com>
2020-09-04 15:51:21 -0400

    Fixed logging and learning rate scheduler

--- GAN_train.py
+++ GAN_train.py
@@ -39,8 +39,12 @@ def main(args):
 	netD.apply(weights_init)
 
 	# 2 optimizers
-	optimizerG = torch.optim.Adam(netG.parameters(), lr=args.lr,betas=(0.5, 0.999))
-	optimizerD = torch.optim.Adam(netD.parameters(), lr=args.lr*3,betas=(0.5, 0.999))
+	optimizerG = torch.optim.Adam(netG.parameters(), lr=args.lrG,betas=(0.5, 0.999))
+	optimizerD = torch.optim.Adam(netD.parameters(), lr=args.lrD,betas=(0.5, 0.999))
+
+	# Schedulers to reduce the learning rate
+	schedulerG = torch.optim.lr_scheduler.StepLR(optimizerG, 10, gamma=0.5)
+	schedulerD = torch.optim.lr_scheduler.StepLR(optimizerD, 10, gamma=0.5)
 
 	logging.info(f"Built a generator model consisting of {sum(p.numel() for p in netG.parameters()):,} parameters")
 	logging.info(f"Built a discriminator model consisting of {sum(p.numel() for p in netD.parameters()):,} parameters")
@@ -67,15 +71,14 @@ def main(args):
 	criterion = torch.nn.BCELoss()
 
 	# Track moving average of loss values
-	train_meters = {name: utils.RunningAverageMeter(0.98) for name in (["train_loss", "train_psnr", "train_ssim"])}
-	valid_meters = {name: utils.AverageMeter() for name in (["valid_psnr", "valid_ssim"])}
+	train_meters = {name: utils.RunningAverageMeter(0.98) for name in ([\
+	                                    "G_loss","G_loss_L2","G_loss_adv","accD","accD_fake","accD_real"])}
+	# valid_meters = {name: utils.AverageMeter() for name in (["Loss"])}
 	writer = SummaryWriter(log_dir=args.experiment_dir) if not args.no_visual else None
 
-
 	# TRAINING
-	fake_label = 0.1
+	fake_label = 0.
 	real_label = 1.
-	real_labelD = 0.9
 
 	G_losses = []
 	D_losses = []
@@ -104,7 +107,7 @@ def main(args):
 	        # Only update discriminator based on update ratio
 	        real_cpu = real.to(device)
 	        b_size = real_cpu.size(0)
-	        label = torch.full((b_size,),real_labelD,device=device)
+	        label = torch.full((b_size,),real_label,device=device)
 
 	        if batch_id % args.g_d_update_ratio == 0:
 	            netD.zero_grad()
@@ -116,6 +119,9 @@ def main(args):
 	            # Calculate gradients for D in backward pass
 	            errD_real.backward()
 	            D_x = output.mean().item()
+	            # Accuracy is  the number of 1's when all samples are real
+	            preds = [1 if o > 0.5 else 0 for o in output]
+	            accD_real = np.sum(preds)/b_size
 	        
 	        ## Train with all-fake batch
 	        # Generate fake signal batch with G
@@ -131,11 +137,18 @@ def main(args):
 	            output = netD(fake.detach()).view(-1)
 	            # Calculate D's loss on the all-fake batch
 	            errD_fake = criterion(output, label)
+
+	            # Accuracy is  the number of 1's when all samples are fake
+	            # Note I flipped the pred logic for fakes
+	            preds = [1 if o < 0.5 else 0 for o in output]
+	            accD_fake = np.sum(preds)/b_size
+	            accD = (accD_fake+accD_real)/2
 	            # Calculate the gradients for this batch
 	            errD_fake.backward()
 	            D_G_z1 = output.mean().item()
 	            # Add the gradients from the all-real and all-fake batches
 	            errD = errD_real + errD_fake
+
 	            # Update D
 	            optimizerD.step()
 	        
@@ -154,15 +167,17 @@ def main(args):
 	            errG_l2 = F.mse_loss(fake, inputs, reduction="sum") / (inputs.size(0) * 2)
 	            errG =  (1-args.wtl2) * errG_D + args.wtl2 * errG_l2
 	        else:
+	            errG_l2 = torch.zeros(1)
 	            errG = criterion(output, label)
-	        
+	            errG_D = errG
 	        # Calculate gradients for G
 	        errG.backward()
 	        D_G_z2 = output.mean().item()
 	        # Update G
 	        optimizerG.step()
 	        
-	       
+
+	        
 	        # Output training stats
 	        if batch_id % 50 == 0:
 	            if use_mse:
@@ -182,50 +197,66 @@ def main(args):
 	        
 	        # TO DO, only run loss on masked part of output
 	        # TO DO, incorporate MSE loss into GAN?
-	#         loss = F.mse_loss(outputs, inputs, reduction="sum") / (inputs.size(0) * 2)
+	        # loss = F.mse_loss(outputs, inputs, reduction="sum") / (inputs.size(0) * 2)
+	        
+	        # New train_meters
+	        train_meters["G_loss"].update(errG.item())
+	        if use_mse:
+	            train_meters["G_loss_L2"].update(errG_l2.item())
+	            train_meters["G_loss_adv"].update(errG_D.item())
+	        else:
+	            train_meters["G_loss_L2"].update(0)
+	            train_meters["G_loss_adv"].update(0)
+	        train_meters["accD"].update(accD.item())
+	        train_meters["accD_fake"].update(accD_fake.item())
+	        train_meters["accD_real"].update(accD_real.item())
 
-	        train_psnr = utils.psnr(fake, inputs)
-	        train_ssim = utils.ssim(fake, inputs)
-	        train_meters["train_loss"].update(errG.item())
-	        train_meters["train_psnr"].update(train_psnr.item())
-	        train_meters["train_ssim"].update(train_ssim.item())
 	        train_bar.log(dict(**train_meters, lr=optimizerG.param_groups[0]["lr"]), verbose=True)
 
 	        if writer is not None and global_step % args.log_interval == 0:
+	            print("Got into the writer!: {} {}".format(errG.item(),accD_real.item()))
 	            writer.add_scalar("lr", optimizerG.param_groups[0]["lr"], global_step)
-	            writer.add_scalar("loss/train", errG.item(), global_step)
-	            writer.add_scalar("psnr/train", train_psnr.item(), global_step)
-	            writer.add_scalar("ssim/train", train_ssim.item(), global_step)
+	            writer.add_scalar("G_loss/train", errG.item() , global_step)
+	            writer.add_scalar("G_loss_L2/train", errG_l2.item() , global_step)
+	            writer.add_scalar("G_loss_adv/train", errG_D.item() , global_step)
+	            writer.add_scalar("accD/train", accD.item() , global_step)
+	            writer.add_scalar("accD_fake/train", accD_fake.item() , global_step)
+	            writer.add_scalar("accD_real/train", accD_real.item() , global_step)
 	            gradients = torch.cat([p.grad.view(-1) for p in netG.parameters() if p.grad is not None], dim=0)
 	            writer.add_histogram("gradients", gradients, global_step)
 	            sys.stdout.flush()
 
-	    if epoch % args.valid_interval == 0:
-	        netG.eval()
-	        for meter in valid_meters.values():
-	            meter.reset()
+	# Rewrite validation loop for the right metrics
+	    # if epoch % args.valid_interval == 0:
+	#         netG.eval()
+	#         for meter in valid_meters.values():
+	#             meter.reset()
 
-	        valid_bar = utils.ProgressBar(valid_loaderG)
+	#         valid_bar = utils.ProgressBar(valid_loaderG)
 	        
-	        for sample_id, (clean, mask) in enumerate(valid_bar):
-	            with torch.no_grad():
-	                inputs = clean.to(device)
-	                mask_inputs = mask.to(device)
-	                # only use the mask part of the outputs
-	                raw_output = netG(inputs,mask_inputs)
-	                output = (1-mask_inputs)*raw_output + mask_inputs*inputs
-	                valid_psnr = utils.psnr(inputs, output)
-	                valid_meters["valid_psnr"].update(valid_psnr.item())
-	                valid_ssim = utils.ssim(inputs, output)
-	                valid_meters["valid_ssim"].update(valid_ssim.item())
+	#         for sample_id, (clean, mask) in enumerate(valid_bar):
+	#             with torch.no_grad():
+	#                 inputs = clean.to(device)
+	#                 mask_inputs = mask.to(device)
+	#                 # only use the mask part of the outputs
+	#                 raw_output = netG(inputs,mask_inputs)
+	#                 output = (1-mask_inputs)*raw_output + mask_inputs*inputs
+	#                 valid_psnr = utils.psnr(inputs, output)
+	#                 valid_meters["valid_psnr"].update(valid_psnr.item())
+	#                 valid_ssim = utils.ssim(inputs, output)
+	#                 valid_meters["valid_ssim"].update(valid_ssim.item())
 
-	        if writer is not None:
-	            writer.add_scalar("psnr/valid", valid_meters['valid_psnr'].avg, global_step)
-	            writer.add_scalar("ssim/valid", valid_meters['valid_ssim'].avg, global_step)
-	            sys.stdout.flush()
-	            utils.save_checkpoint_GAN(args, global_step, netG, netD, optimizerG, optimizerD, score=valid_meters["valid_psnr"].avg, mode="max")
+	#         if writer is not None:
+	#             writer.add_scalar("psnr/valid", valid_meters['valid_psnr'].avg, global_step)
+	#             writer.add_scalar("ssim/valid", valid_meters['valid_ssim'].avg, global_step)
+	#             sys.stdout.flush()
+	#             utils.save_checkpoint_GAN(args, global_step, netG, netD, optimizerG, optimizerD, score=valid_meters["valid_psnr"].avg, mode="max")
 
-	        logging.info(train_bar.print(dict(**train_meters, **valid_meters, lr=optimizerG.param_groups[0]["lr"])))
+	    if epoch % args.log_interval == 0:
+	        logging.info(train_bar.print(dict(**train_meters,lr=optimizerG.param_groups[0]["lr"])))
+
+	    schedulerG.step()
+	    schedulerD.step()
 	# Save the loss curve plot
 	utils.save_losses_curve(G_losses,D_losses,args)
 	# logging.info(f"Done training! Best PSNR {utils.save_checkpoint_GAN.best_score:.3f} obtained after step {utils.save_checkpoint.best_step}.")
@@ -252,8 +283,9 @@ def get_args():
 	parser.add_argument("--g_d_update_ratio", default = 2, type=int, help="How many times to update G for each update of D")
 
 	# Add optimization arguments
-	parser.add_argument("--lr", default=.0005, type=float, help="learning rate")
-	parser.add_argument("--num-epochs", default=300, type=int, help="force stop training at specified epoch")
+	parser.add_argument("--lrG", default=.0004, type=float, help="learning rate for generator")
+	parser.add_argument("--lrD", default=.001, type=float, help="learning rate for discriminator")
+	parser.add_argument("--num-epochs", default=1000, type=int, help="force stop training at specified epoch")
 	parser.add_argument("--valid-interval", default=25, type=int, help="evaluate every N epochs")
 	parser.add_argument("--save-interval", default=1, type=int, help="save a checkpoint every N steps")
 
